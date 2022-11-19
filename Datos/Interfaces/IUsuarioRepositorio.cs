b8d5cd58e93950abaf6bdbe4297c42b1505826a7
Gloriagonzales22 <gigonzales@unah.hn>
2022-11-19 13:22:25 -0600

    Agregar archivos de proyecto.

---
+++ Datos/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using Modelos;
+
+namespace Datos.Interfaces
+{
+    public interface IUsuarioRepositorio
+    {
+        Task<Usuario> GetPorCodigo(string Codigo);
+        Task<bool> Nuevo(Usuario usuario);
+        Task<bool> Actualizar(Usuario usuario);
+        Task<bool> Eliminar(string Codigo);
+        Task<IEnumerable<Usuario>> GetLista();
+    }
+}
